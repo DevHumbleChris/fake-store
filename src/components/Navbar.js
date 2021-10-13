cb5e79eec1ebeee6329a23b7b8392cf77b587db2
DevHumbleChris <dev.humblechris254@gmail.com>
2021-10-13 16:23:45 +0300

    Updated the routes and the show cart modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,131 +3,14 @@ import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import { MenuIcon, SearchIcon, ShoppingBagIcon, XIcon } from '@heroicons/react/outline'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
+import ShoppingCart from './ShoppingCart'
 
-const navigation = {
-  categories: [
-    {
-      id: 'women',
-      name: 'Women',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-          imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
-        },
-        {
-          name: 'Basic Tees',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        },
-      ],
-      sections: [
-        {
-          id: 'clothing',
-          name: 'Clothing',
-          items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Dresses', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Denim', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
-          ],
-        },
-        {
-          id: 'accessories',
-          name: 'Accessories',
-          items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Significant Other', href: '#' },
-          ],
-        },
-      ],
-    },
-    {
-      id: 'men',
-      name: 'Men',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-          imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-        },
-        {
-          name: 'Artwork Tees',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
-          imageAlt:
-            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-        },
-      ],
-      sections: [
-        {
-          id: 'clothing',
-          name: 'Clothing',
-          items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
-          ],
-        },
-        {
-          id: 'accessories',
-          name: 'Accessories',
-          items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-          ],
-        },
-      ],
-    },
-  ],
-  pages: [
+const pages = [
     { name: "Men's Clothing", href: '/mens-clothing', fontIcon: ['fas', 'male'] },
     { name: "Women's Clothing", href: '/womens-clothing', fontIcon: ['fas', 'female']  },
     { name: "Electronics", href: '/electronics', fontIcon: ['fas', 'tv']  },
     { name: "Jewellery", href: '/jewelery', fontIcon: ['fas', 'gem']  },
-  ],
-}
+  ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -135,6 +18,15 @@ function classNames(...classes) {
 
 function Navbar() {
   const [open, setOpen] = useState(false)
+  const [isShoppingCart, setIsShoppingCart] = useState(false)
+
+  const showShoppingCart = () => {
+    setIsShoppingCart(true)
+  }
+
+  const closeShoppingCart = () => {
+    setIsShoppingCart(false)
+  }
 
   return (
     <div className="bg-white sticky">
@@ -192,9 +84,13 @@ function Navbar() {
               {/* Links */}
 
               <div className="border-t border-gray-200 py-6 px-4 space-y-6">
-                {navigation.pages.map((page) => (
+                {pages.map((page) => (
                   <div key={page.name} className="flow-root">
-                    <Link to={page.href} className="-m p-2 block font-medium text-gray-900">
+                    <Link
+                      to={page.href}
+                      className="-m p-2 block font-medium text-gray-900"
+                      onClick={() => setOpen(false)}
+                    >
                       <FontAwesomeIcon icon={page.fontIcon} className="mx-2 text-xl"/>
                       {console.log(page.fontIcon)}
                       {page.name}
@@ -260,7 +156,6 @@ function Navbar() {
                 <Link
                   to="/"
                   className="flex items-center"
-                  onClick={() => setOpen(false)}
                 >
                   <img
                     className="h-8 w-auto"
@@ -274,99 +169,14 @@ function Navbar() {
               {/* Flyout menus */}
               <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
                 <div className="h-full flex space-x-8">
-                  {navigation.categories.map((category) => (
-                    <Popover key={category.name} className="flex">
-                      {({ open }) => (
-                        <>
-                          <div className="relative flex">
-                            <Popover.Button
-                              className={classNames(
-                                open
-                                  ? 'border-indigo-600 text-indigo-600'
-                                  : 'border-transparent text-gray-700 hover:text-gray-800',
-                                'relative z-10 flex items-center transition-colors ease-out duration-200 text-sm font-medium border-b-2 -mb-px pt-px'
-                              )}
-                            >
-                              {category.name}
-                            </Popover.Button>
-                          </div>
-
-                          <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-200"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                            leave="transition ease-in duration-150"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                          >
-                            <Popover.Panel className="absolute top-full inset-x-0 text-sm text-gray-500">
-                              {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                              <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
-
-                              <div className="relative bg-white">
-                                <div className="max-w-7xl mx-auto px-8">
-                                  <div className="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
-                                    <div className="col-start-2 grid grid-cols-2 gap-x-8">
-                                      {category.featured.map((item) => (
-                                        <div key={item.name} className="group relative text-base sm:text-sm">
-                                          <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden group-hover:opacity-75">
-                                            <img
-                                              src={item.imageSrc}
-                                              alt={item.imageAlt}
-                                              className="object-center object-cover"
-                                            />
-                                          </div>
-                                          <a href={item.href} className="mt-6 block font-medium text-gray-900">
-                                            <span className="absolute z-10 inset-0" aria-hidden="true" />
-                                            {item.name}
-                                          </a>
-                                          <p aria-hidden="true" className="mt-1">
-                                            Shop now
-                                          </p>
-                                        </div>
-                                      ))}
-                                    </div>
-                                    <div className="row-start-1 grid grid-cols-3 gap-y-10 gap-x-8 text-sm">
-                                      {category.sections.map((section) => (
-                                        <div key={section.name}>
-                                          <p id={`${section.name}-heading`} className="font-medium text-gray-900">
-                                            {section.name}
-                                          </p>
-                                          <ul
-                                            role="list"
-                                            aria-labelledby={`${section.name}-heading`}
-                                            className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                          >
-                                            {section.items.map((item) => (
-                                              <li key={item.name} className="flex">
-                                                <a href={item.href} className="hover:text-gray-800">
-                                                  {item.name}
-                                                </a>
-                                              </li>
-                                            ))}
-                                          </ul>
-                                        </div>
-                                      ))}
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </Popover.Panel>
-                          </Transition>
-                        </>
-                      )}
-                    </Popover>
-                  ))}
-
-                  {navigation.pages.map((page) => (
-                    <a
+                  {pages.map((page) => (
+                    <Link
                       key={page.name}
-                      href={page.href}
+                      to={page.href}
                       className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
                     >
                       {page.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </Popover.Group>
@@ -382,18 +192,6 @@ function Navbar() {
                   </a>
                 </div>
 
-                <div className="hidden lg:ml-8 lg:flex">
-                  <a href="#" className="text-gray-700 hover:text-gray-800 flex items-center">
-                    <img
-                      src="https://tailwindui.com/img/flags/flag-canada.svg"
-                      alt=""
-                      className="w-5 h-auto block flex-shrink-0"
-                    />
-                    <span className="ml-3 block text-sm font-medium">CAD</span>
-                    <span className="sr-only">, change currency</span>
-                  </a>
-                </div>
-
                 {/* Search */}
                 <div className="flex lg:ml-6">
                   <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
@@ -404,14 +202,18 @@ function Navbar() {
 
                 {/* Cart */}
                 <div className="ml-4 flow-root lg:ml-6">
-                  <a href="#" className="group -m-2 p-2 flex items-center">
+                  { isShoppingCart && <ShoppingCart closeCart={closeShoppingCart} /> }
+                  <button
+                    className="group -m-2 p-2 flex items-center"
+                    onClick={showShoppingCart}
+                  >
                     <ShoppingBagIcon
                       className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
                       aria-hidden="true"
                     />
                     <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                     <span className="sr-only">items in cart, view bag</span>
-                  </a>
+                  </button>
                 </div>
               </div>
             </div>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,37 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function ProductList({ products }) {
+  const [isAddToCart, setIsAddToCart] = useState(false)
+  const [currentCart, setCurrentCart] = useState([])
+  const addToCart = (product) => {
+    setIsAddToCart(true)
+    setCurrentCart([...currentCart, product.id])
+  }
   return (
     <div className="bg-white">
-      <div className="max-w-2xl mx-auto py-4 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="max-w-xl mx-auto py-4 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <div key={product.id} className="group relative bg-gray-300 rounded p-2 shadow-xl">
-              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  className="w-full h-auto object-center object-cover lg:w-full lg:h-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-pink-700">
-                    <a href="#">
-                      <span aria-hidden="true" className="absolute inset-0 font-extrabold" />
-                      {product.title}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+              <div className="max-w-xl max-h-xs">
+                <img src={product.image} className="rounded-xl object-contain" />
+                <div className="p-2">
+                  <p className="text-sm font-extrabold">
+                    { product.title }
+                  </p>
+                  <span className="text-sm text-gray-500">
+                    { product.category }
+                  </span>
+                  <span className="float-right text-blue-900">
+                    $ { product.price }
+                  </span>
                 </div>
-                <p className="text-sm font-medium text-blue-900 mx-2">{product.price}</p>
               </div>
-              <button className="bg-pink-500 w-full text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-2 ease-linear transition-all duration-150" type="button"
-              >
-                <FontAwesomeIcon icon={['fas', 'cart-plus']} /> Add to cart
-              </button>
+              {
+                isAddToCart ?
+                currentCart === product.id ?
+                <div className="flex justify-between items-center">
+                  <button
+                    className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-2 ease-linear transition-all duration-150 cursor-pointer"
+                    type="button"
+                    onClick={() => addToCart(product)}
+                  >
+                    <FontAwesomeIcon icon={['fas', 'plus']} />
+                  </button>
+                  <p>
+                    { 5 }
+                  </p>
+                  <button
+                    className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-2 ease-linear transition-all duration-150 cursor-pointer"
+                    type="button"
+                    onClick={() => addToCart(product)}
+                  >
+                    <FontAwesomeIcon icon={['fas', 'minus']} />
+                  </button>
+                </div>
+                :
+                <button
+                  className="bg-pink-500 w-full text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-2 ease-linear transition-all duration-150 cursor-pointer"
+                  type="button"
+                  onClick={() => addToCart(product)}
+                  >
+                  <FontAwesomeIcon icon={['fas', 'cart-plus']} /> Add to cart
+                  </button>
+                :
+                <button
+                  className="bg-pink-500 w-full text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-2 ease-linear transition-all duration-150 cursor-pointer"
+                  type="button"
+                  onClick={() => addToCart(product)}
+                  >
+                  <FontAwesomeIcon icon={['fas', 'cart-plus']} /> Add to cart
+                  </button>
+              }
             </div>
           ))}
         </div>
